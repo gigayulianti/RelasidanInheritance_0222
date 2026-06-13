9930574ec1a64947c74854418292bd49ca865c8f
gigayulianti <yulianti2@gmail.com>
2026-06-13 18:51:48 +0700

    commit 3

--- anak.h.cpp
+++ anak.h.cpp
@@ -9,13 +9,4 @@ public:
     //deklarsi member variabel pada class anak
     string nama;
     
-    //membuat constructor dan descructor pada  class anak
-    anak(string pNama) : nama(pNama) {
-        cout << "Anak \"" << nama << "\" ada \n";
-    }
-
-    ~anak() {
-        cout << "Anak \"" << nama << "\" tidak ada\n";
-    }
-};
-#endif
+    
